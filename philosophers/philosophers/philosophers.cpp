e200253e31d5412235f58ef04ab3fa6a572c5c65
Manuel <manuelgalvez192@gmail.com>
2024-02-08 00:04:39 +0100

    sigue tu

--- philosophers/philosophers/philosophers.cpp
+++ philosophers/philosophers/philosophers.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <thread>
+
+using namespace std;
+
+void funcion(const char *str)
+{
+	while (1)
+	{
+		cout << str;
+		this_thread::sleep_for(chrono::seconds(1));
+	}
+}
+
+void funcion2(const int num)
+{
+	while (1)
+	{
+		cout << num;
+		this_thread::sleep_for(chrono::seconds(1));
+	}
+}
 
 int main(int arg, char **argv)
 {
-	
+	thread thread1(funcion, "hilo 1\n");
+	thread thread2(funcion, "hilo 2\n");
+	thread1.join();
+	thread2.join();
 	//la vix manda
 	return 0;
 }

07741033b37106841915e63bcbb173a95777c838
Manuel <manuelgalvez192@gmail.com>
2024-02-10 19:27:10 +0100

    confio en ti <3

--- philosophers/philosophers.cpp
+++ philosophers/philosophers.cpp
@@ -1,17 +1,18 @@
 #include "PhiloHeader.h"
 
-void *philo_tasks(void *var)
+void *philo_tasks(int fork_id)
 {
-	cout << "manu es virgen" << endl;
+	
 }
 
 int main(int argc, char **argv)
 {
 	int num_philo = 5;
 	pthread_t philo[num_philo];
+	int time_to_die = 5000, time_to_eat = 1000, time_to_sleep = 1000;
 	int num_fork[num_philo];
 
-	for (int i = 0; i < num_philo; i++){
+	for (int i = 1; i <= num_philo; i++){
 		num_fork[i] = i;
 		pthread_create(&philo[i], NULL, philo_tasks, &num_fork[i]);
 	}

691ed6ce8d1867fe11039ef35ca7cd46c4c3be08
Vix <vac3wxd@virginia.edu>
2024-02-12 03:56:31 +0100

    el programa funciona, algunos cambios

--- philosophers/philosophers.cpp
+++ philosophers/philosophers.cpp
@@ -1,45 +1,73 @@
 #include <iostream>
 #include <pthread.h>
 #include <unistd.h>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
 
 #define NUM_PHILOSOPHERS 5
 #define NUM_EAT_COUNT 5
-
-using namespace std;
 
 pthread_mutex_t forks[NUM_PHILOSOPHERS];
 pthread_t philosophers[NUM_PHILOSOPHERS];
 int eat_counter[NUM_PHILOSOPHERS] = {0};
 pthread_mutex_t eat_counter_mutex;
+bool philosopher_dead[NUM_PHILOSOPHERS] = {false};
+pthread_mutex_t print_mutex;
+
+void print_state(int philosopher_id, const string &state)
+{
+	auto now = chrono::system_clock::now();
+	auto elapsed = chrono::duration_cast<chrono::milliseconds>(now.time_since_epoch()).count();
+
+	pthread_mutex_lock(&print_mutex);
+	cout << elapsed << " Philosopher " << philosopher_id << " " << state << endl;
+	pthread_mutex_unlock(&print_mutex);
+}
 
 void *philosopher(void *arg)
 {
 	int philosopher_id = *((int *)arg);
 	int left_fork = philosopher_id;
 	int right_fork = (philosopher_id + 1) % NUM_PHILOSOPHERS;
+	int first_fork = (philosopher_id == NUM_PHILOSOPHERS - 1) ? right_fork : left_fork;
+	int second_fork = (philosopher_id == NUM_PHILOSOPHERS - 1) ? left_fork : right_fork;
 
-	for (int i = 0; i < NUM_EAT_COUNT; ++i)
+	for (int i = 0; i < NUM_EAT_COUNT && !philosopher_dead[philosopher_id]; ++i)
 	{
 		// Thinking
-		cout << "Philosopher " << philosopher_id << " is thinking." << endl;
+		print_state(philosopher_id, "is thinking");
 		sleep(1);
 
-		// Pick up/Lock forks
-		pthread_mutex_lock(&forks[left_fork]);
-		pthread_mutex_lock(&forks[right_fork]);
+		// Pick up forks
+		pthread_mutex_lock(&forks[first_fork]);
+		print_state(philosopher_id, "has taken a fork");
+		if (pthread_mutex_trylock(&forks[second_fork]) != 0)
+		{
+			pthread_mutex_unlock(&forks[first_fork]);
+			continue; // Try again later if the second fork is not available
+		}
+		print_state(philosopher_id, "has taken a fork");
 
 		// Eating
-		cout << "Philosopher " << philosopher_id << " is eating." << endl;
+		print_state(philosopher_id, "is eating");
 		sleep(1);
 
-		// Release/Unlock forks
-		pthread_mutex_unlock(&forks[left_fork]);
-		pthread_mutex_unlock(&forks[right_fork]);
+		// Release forks
+		pthread_mutex_unlock(&forks[first_fork]);
+		pthread_mutex_unlock(&forks[second_fork]);
 
 		// Increment eat counter
 		pthread_mutex_lock(&eat_counter_mutex);
 		eat_counter[philosopher_id]++;
 		pthread_mutex_unlock(&eat_counter_mutex);
+	}
+
+	if (!philosopher_dead[philosopher_id])
+	{
+		print_state(philosopher_id, "died");
+		philosopher_dead[philosopher_id] = true;
 	}
 
 	return NULL;
@@ -55,6 +83,9 @@ int main()
 
 	// Initialize eat counter mutex
 	pthread_mutex_init(&eat_counter_mutex, NULL);
+
+	// Initialize print mutex
+	pthread_mutex_init(&print_mutex, NULL);
 
 	// Create philosopher threads
 	int philosopher_ids[NUM_PHILOSOPHERS];
@@ -78,6 +109,9 @@ int main()
 
 	// Destroy eat counter mutex
 	pthread_mutex_destroy(&eat_counter_mutex);
+
+	// Destroy print mutex
+	pthread_mutex_destroy(&print_mutex);
 
 	cout << "All philosophers are happy and have eaten" << endl;
 
